908d3a0cea3ff5beb404d4cf98efbea805ae655c
Lucas Alves <lucasfg907@gmail.com>
2026-01-24 21:25:35 -0400

    Feature add create and update user DTOs with validation

--- src/main/java/com/lucas_alves/helpdesk/mapper/UserMapper.java
+++ src/main/java/com/lucas_alves/helpdesk/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.lucas_alves.helpdesk.mapper;
 
 import controller.dto.CreateUserDto;
+import controller.dto.UpdateUserDto;
 import model.User;
 import org.springframework.stereotype.Component;
 
@@ -16,5 +17,16 @@ public class UserMapper {
         user.setEmail(createUserDto.email());
 
         return user;
+    }
+    public void toEntity(UpdateUserDto updateUserDto, User user) {
+
+        user.setName(updateUserDto.name());
+        user.setBirthdayDate(updateUserDto.birthdayDate());
+        user.setCpf(updateUserDto.cpf());
+        user.setRg(updateUserDto.rg());
+        user.setEmail(updateUserDto.email());
+        user.setSector(updateUserDto.sector());
+        user.setRole(updateUserDto.role());
+
     }
 }
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -2,14 +2,18 @@ package service;
 
 import com.lucas_alves.helpdesk.mapper.UserMapper;
 import controller.dto.CreateUserDto;
+import controller.dto.UpdateUserDto;
+import jakarta.validation.Valid;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import model.User;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import repository.UserRepository;
 
-import java.time.LocalDate;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -31,6 +35,16 @@ public class UserService {
         }
 
         User user = mapper.toEntity(createUserDto);
+
+        return userRepository.save(user);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    public User updateUser(UpdateUserDto updateUserDto) {
+
+        User user = userRepository.findById(updateUserDto.id()).orElseThrow(() -> new RuntimeException("User not found"));
+
+        mapper.toEntity(updateUserDto, user);
 
         return userRepository.save(user);
     }
